3cc2c718b6ea2a557500dc9f07de398760c4e33e
ronald-hood-rdc <ronald.hood@realtor.com>
2024-04-05 14:13:05 -0500

    Web Server

--- oracle.sh
+++ oracle.sh
@@ -3,15 +3,26 @@
 # Format the Rust Code
 cargo fmt
 
+# Name of the Docker container
+container_name="oracledb_container"
+
 # Build the Docker image
 docker build . -t oracledb
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Docker image built successfully."
-    # Run the Docker container using the .env file for environment variables
-    docker run --env-file .env -v $(pwd):/usr/src/oracledb oracledb:latest
-
+    
+    # Check if the container already exists
+    if [ $(docker ps -a -q -f name=^/${container_name}$) ]; then
+        # Container exists, so just start it
+        echo "Starting existing container."
+        docker start ${container_name}
+    else
+        # Container does not exist, run a new container
+        echo "Running a new container."
+        docker run -p 8080:8080 --env-file .env -v $(pwd):/usr/src/oracledb --name ${container_name} oracledb:latest
+    fi
 else
     echo "Docker image build failed."
 fi
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,33 @@
+use actix_web::{get, web, App, HttpServer, Responder};
 use dotenv::dotenv;
 use oracle::{
     pool::{self, Pool},
-    Error, RowValue,
+    Connection, Error,
 };
 use std::env;
-use std::fs::File;
-use std::io::Write;
-const LIST_VIEWS_QUERY: &str = "SELECT view_name FROM all_views";
+use std::sync::Arc;
 
-// Define a struct to hold the row data
-#[derive(Debug, RowValue)]
-struct MlsContactsV2 {
-    mls_account_party_id: Option<f64>, // Using Option<> to handle possible NULL values
-    mls_formal_name: Option<String>,
-    mls_id: Option<String>,
-    ir_rep: Option<String>,
-    mls_internet_name: Option<String>,
-    state: Option<String>,
-    mls_account_fulfillment_id: Option<String>,
-    contact_info: Option<String>,
-    doc_id: Option<f64>,
+#[get("/parties/{party_id}")]
+async fn index(web::Path(party_id): web::Path<u32>, pool: web::Data<Arc<Pool>>) -> impl Responder {
+    let conn = pool
+        .get_ref()
+        .get()
+        .expect("Failed to get connection from pool");
+    get_party_info(party_id, &conn).await;
+    format!("Hello {}! id", party_id)
 }
 
-// Adjusted query to select specified fields from MLS_CONTACTS_V2 and limit to first 5 rows
-const LIST_MLS_CONTACTS_QUERY: &str = "
-SELECT MLS_ACCOUNT_PARTY_ID, MLS_FORMAL_NAME, MLS_ID, IR_REP, MLS_INTERNET_NAME, 
-       STATE, MLS_ACCOUNT_FULFILLMENT_ID, CONTACT_INFO, DOC_ID 
-FROM MLS_CONTACTS_V2 
-FETCH FIRST 5 ROWS ONLY";
-
-fn main() -> Result<(), Error> {
+#[actix_web::main]
+async fn main() -> Result<(), Error> {
     dotenv().ok();
     let db_url = construct_db_url().unwrap();
-    let pool = create_connection_pool(&db_url)?;
-    let conn = pool.get()?;
-    let mut file = File::create("views.txt").expect("error");
+    let pool = Arc::new(create_connection_pool(&db_url)?);
 
-    /*let mut stmt = conn.statement(LIST_VIEWS_QUERY).build().unwrap();
-    let rows = stmt.query(&[]).unwrap();
-
-
-    for row_result in rows {
-        let row: oracle::Row = row_result?; // Handle the Result from iterating over rows
-        let view_name: String = row.get("view_name")?; // Extract the view_name column
-
-
-    }
-
-    //list_views(&conn);
-
-    /*  let mut stmt = conn.statement(LIST_VIEWS_QUERY).build()?;
-    let rows = stmt.query_as::<MlsContactsV2>(&[])?;
-
-    for row_result in rows {
-        let row: MlsContactsV2 = row_result?;
-        println!("{:?}", row);
-    }*/
-
-    const GET_COLUMN_NAMES_QUERY: &str = "
-        SELECT COLUMN_NAME
-        FROM ALL_TAB_COLUMNS
-        WHERE TABLE_NAME = 'PARTY_INFO_V2_V'";
-    const GET_FIRST_ROWS_QUERY: &str = "
-        SELECT *
-        FROM PARTY_INFO_V2_V
-        FETCH FIRST 5 ROWS ONLY";
-
-    // Query and display column names for PARTY_INFO_V2_V
-    let mut stmt = conn.statement(GET_COLUMN_NAMES_QUERY).build().unwrap();
-    let cols = stmt.query(&[]).unwrap();
-
-    println!("Column Names for PARTY_INFO_V2_V:");
-    for col_result in cols {
-        let col: oracle::Row = col_result?;
-        let column_name: String = col.get("COLUMN_NAME")?;
-        println!("{}", column_name);
-    }
-    // here*/
-    let sql = "select party_id,party_info from CCD.party_info_v2_v";
-    //et sql =
-    //"SELECT * FROM DBA_TAB_PRIVS WHERE TABLE_NAME = 'PARTY_INFO_V2_V' AND PRIVILEGE = 'SELECT'"; //"SELECT VIEW_NAME FROM ALL_VIEWS WHERE VIEW_NAME = 'PARTY_INFO_V2_V'";
-    // Query and display the first few rows of PARTY_INFO_V2_V
-    let mut stmt = conn.statement(sql).build().unwrap();
-    let rows = stmt.query(&[])?;
-
-    println!("\nFirst Few Rows of PARTY_INFO_V2_V:");
-    for row_result in rows {
-        // Here we handle the Result type for each row
-        let row = row_result?;
-        // Now, safely extract the expected tuple from the row
-
-        writeln!(&mut file, "View Name: {:?}", row).expect("msg");
-    }
+    let _ = HttpServer::new(move || App::new().data(Arc::clone(&pool)).service(index))
+        .bind("0.0.0.0:8080")
+        .expect("Failed to bind to 0.0.0.0:8080")
+        .run()
+        .await;
 
     Ok(())
 }
@@ -100,8 +36,6 @@ fn construct_db_url() -> Result<String, env::VarError> {
     let host_name = env::var("HOST_NAME")?;
     let port = env::var("PORT")?;
     let sid = env::var("SID")?;
-    let stringg = format!("//{}:{}/{}", host_name, port, sid);
-    dbg!(stringg);
     Ok(format!("//{}:{}/{}", host_name, port, sid))
 }
 
@@ -113,16 +47,18 @@ fn create_connection_pool(db_url: &str) -> Result<Pool, Error> {
         .max_connections(20)
         .build()
 }
-// account hierarchy table and account response has a acouple attributes. Look at schema, partyid is the objectid.
-/*
- * subject id is the party id for that account, partyid means that the account might be parent of another account.
- relationship type explains how the subject id is related to the party id, contact or member or parernt etc.
- In CIS, go toa ccount, getch infromation and based on the relationship it foes to the hierarchy table. Trying to fetch from different views then does the stitching work
- If there is a view it will just fetch from the view, graphql may not be optimal.
- Graphql is better at one time fetch rather than stitching things together.
- If try to keep flatten file, it will be overwhelming for the update of the file. Updates the view so that specific informaiton si updated,
- the entire response is then generated lazely.
-not everyone is interested in document type, if yuo could expose partyid. logic will be handled by the client.
-can fetch information for indiviudal account for the mls sets. If they want the entire hierarchy then they can go to legqcy CIS.
 
- */
+async fn get_party_info(party_id: u32, conn: &Connection) {
+    let sql = format!(
+        "select party_id,party_info from CCD.party_info_v2_v where party_id = {}",
+        party_id
+    );
+    let mut stmt = conn.statement(&sql).build().unwrap();
+    let rows = stmt.query(&[]).expect("Failed to execute query");
+
+    println!("\nFirst Few Rows of PARTY_INFO_V2_V:");
+    for row_result in rows {
+        let row = row_result.expect("Failed to get row");
+        dbg!(row);
+    }
+}
